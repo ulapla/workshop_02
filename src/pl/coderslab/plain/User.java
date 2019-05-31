004ae2bc72519d3ac48da939274f76a5c3a3e29d
Elroy Smith <elroysmith@test.pl>
2019-05-31 14:40:12 +0200

    Klasa User pola i metody gettery i settery, konstruktory

--- src/pl/coderslab/plain/User.java
+++ src/pl/coderslab/plain/User.java
@@ -1,4 +1,46 @@
 package pl.coderslab.plain;
 
 public class User {
+
+    private int id;
+    private String name;
+    private String password; //zahashowane
+    private String email; //unikatowy
+
+    public User() {
+    }
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
