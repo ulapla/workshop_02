36bcea0b0ade12ef567ad0afc33318484637f7ad
Ula Placek <ulaplacek@gmail.com>
2019-06-21 09:42:32 +0200

    Group class added

--- src/pl/coderslab/main/Main.java
+++ src/pl/coderslab/main/Main.java
@@ -1,7 +1,9 @@
 package pl.coderslab.main;
 
+import pl.coderslab.dao.ExerciseDao;
 import pl.coderslab.dao.SolutionDao;
 import pl.coderslab.dao.UserDao;
+import pl.coderslab.plain.Exercise;
 import pl.coderslab.plain.Solution;
 import pl.coderslab.plain.User;
 
@@ -52,19 +54,39 @@ public class Main {
 //        userDao.delete(user2.getId());
 
 
-        Solution solution = new Solution("2019-06-02","2019-06-03","nowe rozwiązanie",1,1);
-        SolutionDao solutionDao = new SolutionDao();
-        solutionDao.create(solution);
-        System.out.println(solutionDao.read(solution.getId()));
-        solution.setUpdated("2019-06-19");
-        solutionDao.update(solution);
-        System.out.println(solutionDao.read(solution.getId()));
-        solutionDao.delete(solution.getId());
-        List<Solution> all = solutionDao.findAll();
-        for(Solution sol : all){
-            System.out.println(sol);
-        }
+//        Solution solution = new Solution("2019-06-02","2019-06-03","nowe rozwiązanie",1,1);
+//        SolutionDao solutionDao = new SolutionDao();
+//        solutionDao.create(solution);
+//        System.out.println(solutionDao.read(solution.getId()));
+//        solution.setUpdated("2019-06-19");
+//        solutionDao.update(solution);
+//        System.out.println(solutionDao.read(solution.getId()));
+//        solutionDao.delete(solution.getId());
+//        List<Solution> all = solutionDao.findAll();
+//        for(Solution sol : all){
+//            System.out.println(sol);
+//        }
 
+        Exercise exercise = new Exercise();
+        exercise.setTitle("Zadanie 2");
+        exercise.setDescription("tresc zadania");
+        ExerciseDao exerciseDao = new ExerciseDao();
+        exerciseDao.create(exercise);
+        System.out.println(exerciseDao.read(exercise.getId()));
+        exercise.setDescription("nowa treść zadania");
+        exerciseDao.update(exercise);
+        System.out.println(exerciseDao.read(exercise.getId()));
+        System.out.println("------------");
+        List<Exercise> all = exerciseDao.findAll();
+        for(Exercise exer :all){
+            System.out.println(exer);
+        }
+        exerciseDao.delete(exercise.getId());
+        List<Exercise> allex = exerciseDao.findAll();
+        System.out.println("------------");
+        for(Exercise exer :allex){
+            System.out.println(exer);
+        }
     }
 
 
---
+++ src/pl/coderslab/plain/Group.java
@@ -0,0 +1,30 @@
+package pl.coderslab.plain;
+
+public class Group {
+    private int id;
+    private String name;
+
+    public Group(){
+    }
+
+    public Group(String name){
+        this.name = name;
+    }
+
+    public void setName (String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+}
